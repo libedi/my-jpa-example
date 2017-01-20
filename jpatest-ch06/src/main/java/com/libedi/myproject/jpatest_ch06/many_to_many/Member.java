131141cf13940af0351c10631d3a00e0f2ea0f72
libedi <libedi@gmail.com>
2017-01-20 14:22:00 +0900

    [ch06] 다대다 단방향

---
+++ jpatest-ch06/src/main/java/com/libedi/myproject/jpatest_ch06/many_to_many/Member.java
@@ -0,0 +1,48 @@
+package com.libedi.myproject.jpatest_ch06.many_to_many;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Member {
+	@Id @GeneratedValue
+	@JoinColumn(name = "MEMBER_ID")
+	private String id;
+	
+	private String username;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "MEMBER_PRODUCT",	// 다대다를 다대일, 일대다로 연결해주는 매핑 테이블 지정.
+			joinColumns = @JoinColumn(name = "MEMBER_ID"),
+			inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID")
+			)
+	private List<Product> products = new ArrayList<>();
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+}
--- jpatest-ch06/src/test/java/com/libedi/myproject/jpatest_ch06/TestJpaCh06.java
+++ jpatest-ch06/src/test/java/com/libedi/myproject/jpatest_ch06/TestJpaCh06.java
@@ -1,5 +1,7 @@
 package com.libedi.myproject.jpatest_ch06;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -56,5 +58,36 @@ public class TestJpaCh06 {
 		 * - Team 엔티티를 저장할 때, Team.members의 참조값을 확인해서 MEMBER 테이블의 TEAM_ID 외래키를 업데이트 한다.
 		 * : 일대다 단반향 매핑보다는 다대일 단방향 매핑을 사용하자!
 		 */
+	}
+	
+	/**
+	 * ex>6.15 - 다대다 단방향 저장
+	 * @throws Exception
+	 */
+	@Test
+	public void testSaveManyToManySingleDir() throws Exception{
+		com.libedi.myproject.jpatest_ch06.many_to_many.Product productA = new com.libedi.myproject.jpatest_ch06.many_to_many.Product();
+		productA.setId("ProductA");
+		productA.setName("상품A");
+		em.persist(productA);
+		
+		com.libedi.myproject.jpatest_ch06.many_to_many.Member member1 = new com.libedi.myproject.jpatest_ch06.many_to_many.Member();
+		member1.setId("member1");
+		member1.setUsername("회원1");
+		member1.getProducts().add(productA);	// 연관관계 설정
+		em.persist(member1);
+	}
+	
+	/**
+	 * ex>6.16 - 다대다 단방향 탐색
+	 * @throws Exception
+	 */
+	@Test
+	public void testFindManyToManySingleDir() throws Exception{
+		com.libedi.myproject.jpatest_ch06.many_to_many.Member member = em.find(com.libedi.myproject.jpatest_ch06.many_to_many.Member.class, "member1");
+		List<com.libedi.myproject.jpatest_ch06.many_to_many.Product> products = member.getProducts();	// 객체 그래프 탐색
+		for(com.libedi.myproject.jpatest_ch06.many_to_many.Product product : products){
+			System.out.println("product.name = " + product.getName());
+		}
 	}
 }
